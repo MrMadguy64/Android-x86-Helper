729374a7d6d586eddf76992a1b5041fc6acb7c80
MrMadguy64 <43206926+MrMadguy64@users.noreply.github.com>
2022-12-10 11:17:05 +0300

    Add files via upload

--- startnosync.sh
+++ startnosync.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#Just run everything in fully automated mode
+#Just run everything except sync in fully automated mode
 #Override queries in order to surpress them
 
 #Prepare environment
 ./prepare.sh
-#Download/sync repos
-./sync.sh
+#Download/sync repos - skip this stage
+#./sync.sh
 #Build targets
 ./build.sh
